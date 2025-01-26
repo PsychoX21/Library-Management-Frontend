625469f282981ce81447f46b1c571f5029dbcad7
Chirag <chiragupta2005@gmail.com>
2025-01-26 14:23:00 +0530

    Home page completed and created  empty jsx file for every page and added navbar to book page also

--- Central Library/src/components/BookPage.jsx
+++ Central Library/src/components/BookPage.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import { Navbar } from './Navbar'
 
 export const BookPage = () => {
   return (
+    <>
+    <Navbar page="book"/>
     <div>BookPage</div>
+    </>
   )
 }
