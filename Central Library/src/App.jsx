8d4322f28c92109289223d8d179444f437e14c88
Chirag <chiragupta2005@gmail.com>
2025-01-26 08:42:36 +0530

    created functional navbar

--- Central Library/src/App.jsx
+++ Central Library/src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AuthPage from './components/AuthPage';
 import LoginPage from './components/LoginPage';
 import SignupPage from './components/SignupPage';
+import { Homepage } from './components/Homepage';
+import { MapPage } from './MapPage';
 
 const App = () => {
   return (
@@ -11,6 +13,8 @@ const App = () => {
         <Route path="/" element={<AuthPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
+        <Route path="/home" element={<Homepage />} />
+        <Route path="/map" element={<MapPage />} />
       </Routes>
     </Router>
   );
